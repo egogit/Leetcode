084f26befaf627840ef1e0647eaeed271f3bf69b
ego <86646844+egogit@users.noreply.github.com>
2023-09-12 12:13:10 +0900

    Time: 185 ms (41.68%), Space: 17.3 MB (23.36%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,23 +1,30 @@
+from collections import defaultdict
+
 class Solution:
     def minDeletions(self, s: str) -> int:
-        arr = [0]*26;
+        cnt = defaultdict(int);
         ans = 0;
 
         for a in s:
-            i = ord(a) - 97;
-            arr[i] += 1;
+            cnt[a] += 1;
+
+        arr = list();
+
+        for a in cnt.values():
+            arr.append(a);
         
-        arr.sort(reverse=True);
+        arr.sort(reverse=True)
+
         prev = arr[0];
 
         i = 1;
-        while(i < 26 and arr[i] != 0):
-            if 0 < prev <= arr[i]:
-                ans += (arr[i]-prev+1)
-                arr[i] = prev - 1;
-            elif prev == 0:
+        while(i < len(arr)):
+            if prev == 0:
                 ans += arr[i];
                 arr[i] = 0;
+            elif 0 < prev <= arr[i]:
+                ans += (arr[i]-prev+1)
+                arr[i] = prev - 1;
 
             prev = arr[i];
             i += 1;
